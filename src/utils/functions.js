616dc29ffe23e6ab1eab2e0a08826df13ab2b151
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2023-08-02 11:16:07 +0200

    added a utility function to handle email auth

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,3 +2,29 @@ export default function validateEmail(email) {
   const re = /\S+@\S+\.\S+/;
   return re.test(email);
 }
+
+export const customErrorMessage = (errorCode) => {
+  let message = "";
+
+  switch (errorCode.toString()) {
+    case "EMAIL_EXISTS":
+      message = "Your account is already registered";
+      break;
+    case "INVALID_EMAIL":
+      message = "You have entered an invalid email";
+      break;
+    case "INVALID_PASSWORD":
+      message = "You have entered an invalid password";
+      break;
+    case "TIMEOUT":
+      message = "Registration has timed out";
+      break;
+    case "WEAK_PASSWORD":
+      message = "You have entered a weak password";
+      break;
+    default:
+      message = errorCode;
+  }
+
+  return message;
+};
