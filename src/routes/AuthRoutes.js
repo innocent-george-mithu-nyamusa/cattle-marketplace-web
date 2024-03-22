57b6190ed66381ccdcfac24fa77a0195eb2ad6d8
Isheanesu Gwangwanyu <innocentnyamusa@gmail.com>
2024-03-22 16:44:26 +0200

    cerated list for open access routes

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,18 +1,17 @@
-export default AuthRoutes = 
-    [
-        {
-          name: "about us",
-          route: "/pages/landing-pages/about-us",
-          component: <AboutUs />,
-        },
-        {
-          name: "contact us",
-          route: "/pages/landing-pages/contact-us",
-          component: <ContactUs />,
-        },
-        {
-          name: "author",
-          route: "/pages/landing-pages/author",
-          component: <Author />,
-        },
-      ]
+export default AuthRoutes = [
+  {
+    name: "about us",
+    route: "/pages/landing-pages/about-us",
+    component: <AboutUs />,
+  },
+  {
+    name: "contact us",
+    route: "/pages/landing-pages/contact-us",
+    component: <ContactUs />,
+  },
+  {
+    name: "author",
+    route: "/pages/landing-pages/author",
+    component: <Author />,
+  },
+];
