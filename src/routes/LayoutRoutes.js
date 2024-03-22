aa7cddcbb19b89ade2f4e2e00aac5ba3a222aab6
Isheanesu Gwangwanyu <innocentnyamusa@gmail.com>
2024-03-22 16:43:55 +0200

    routing for in app routes

--- src/routes/LayoutRoutes.js
+++ src/routes/LayoutRoutes.js
@@ -1,1 +1,21 @@
- 
+import { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+import CustomLayout from "layouts/sections/components/CustomLayout";
+
+import routes from "routes/Routes";
+
+const LayoutRoutes = () => {
+  return (
+    <Fragment>
+      <Routes>
+        {routes.map(({ route, component }, i) => (
+          <Route element={<CustomLayout />} key={i}>
+            <Route path={route} element={component} />
+          </Route>
+        ))}
+      </Routes>
+    </Fragment>
+  );
+};
+
+export default LayoutRoutes;
