ed34772d81dec4952be63b5557df736460681fb8
Isheanesu Gwangwanyu <innocentnyamusa@gmail.com>
2024-03-22 16:45:06 +0200

    created private routes filtering

---
+++ src/routes/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { app } from "config/firebase";
+
+const PrivateRoute = () => {
+  const [currentUser, setCurrentUser] = useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    if (isMounted) {
+      console.log(isMounted, " is Mounted");
+      app.auth().onAuthStateChanged(setCurrentUser);
+      setAuthenticated(JSON.parse(localStorage.getItem("authenticated")));
+      localStorage.setItem("authenticated", authenticated);
+      localStorage.setItem("currentUser", currentUser);
+    }
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
+
+  return currentUser !== null ? (
+    <Outlet />
+  ) : (
+    <Navigate exact to={`${process.env.PUBLIC_URL}/login`} />
+  );
+};
+
+export default PrivateRoute;
