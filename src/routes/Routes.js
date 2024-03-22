f2f4121180e42f123803975e7212a0df694adbd1
Isheanesu Gwangwanyu <innocentnyamusa@gmail.com>
2024-03-22 16:47:07 +0200

    updatd list for authenticated routes for access

---
+++ src/routes/Routes.js
@@ -0,0 +1,41 @@
+import Homepage from "layouts/pages/homepage";
+const Routes = [
+  {
+    name: "mashona",
+    route: "/livestock/cattle/mashona",
+    component: <Homepage />,
+    properties: { animalType: "cattle", animalBreed: "mashona", animalName: "cattle" },
+  },
+  {
+    name: "brahman",
+    route: "/livestock/cattle/brahman",
+    component: <Homepage />,
+    properties: { animalType: "cattle", animalBreed: "brahman", animalName: "cattle" },
+  },
+  {
+    name: "hereford",
+    route: "/livestock/cattle/hereford",
+    component: <Homepage />,
+    properties: { animalType: "cattle", animalBreed: "hereford", animalName: "cattle" },
+  },
+  {
+    name: "simmental",
+    route: "/livestock/cattle/simmental",
+    component: <Homepage />,
+    properties: { animalType: "cattle", animalBreed: "simmental", animalName: "cattle" },
+  },
+  {
+    name: "aberdeen-angus",
+    route: "/livestock/cattle/arbedeen-angus",
+    component: <Homepage />,
+    properties: { animalType: "cattle", animalBreed: "arbedeen", animalName: "cattle" },
+  },
+  {
+    name: "charolais",
+    route: "/livestock/cattle/charolais",
+    component: <Homepage />,
+    properties: { animalType: "cattle", animalBreed: "charolais", animalName: "cattle" },
+  },
+];
+
+export default Routes;
