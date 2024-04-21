f1730465af56d5920513b8dbf0b0f567848d30f8
innocent-george-mithu-nymausa <innocentnyamusa@gmail.com>
2024-04-21 13:16:45 +0200

    updated home page

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,6 +2,13 @@ export default function validateEmail(email) {
   const re = /\S+@\S+\.\S+/;
   return re.test(email);
 }
+export function truncateString(str, n, useWordBoundary) {
+  if (str.length <= n) {
+    return str;
+  }
+  const subString = str.slice(0, n - 1); // the original check
+  return (useWordBoundary ? subString.slice(0, subString.lastIndexOf(" ")) : subString) + " ...";
+}
 
 export const customErrorMessage = (errorCode) => {
   let message = "";
