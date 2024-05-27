3a0fecc6fae8c5330d721514f5aab127009ccb1a
Isheanesu Gwangwanyu <innocentnyamusa@gmail.com>
2024-05-27 17:53:04 +0200

    successfully created single product view image

--- src/components/ImageSliderComponent/index.js
+++ src/components/ImageSliderComponent/index.js
@@ -1,42 +1,64 @@
 import Slider from "react-slick";
 import PropTypes from "prop-types";
-import { Media } from "reactstrap";
+// import { Media } from "reactstrap";
 import MKBox from "components/MKBox";
 
-function ImageSliderComponent({ baseUrl }) {
-  const paging = (i) => {
-    console.log(i);
-    return (
-      <>
-        <Media src={`${baseUrl}`} />
-      </>
-    );
-  };
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", textColor: "red", "&::Before": { color: "red" } }}
+      onClick={onClick}
+    />
+  );
+}
 
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", textColor: "green", "&::Before": { color: "red" } }}
+      onClick={onClick}
+    />
+  );
+}
+
+function ImageSliderComponent({ baseUrl }) {
   const settings = {
+    customPaging: function (i) {
+      return (
+        <a>
+          <img height="100%" width="100%" key={i} src={`${baseUrl}`} />
+        </a>
+      );
+    },
     dots: true,
     dotsClass: "slick-dots slick-thumb",
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
 
   return (
-    <MKBox className="slider-container">
-      <Slider customPaging={paging} {...settings}>
-        <MKBox>
-          <Media src={baseUrl} />
-        </MKBox>
-        <MKBox>
-          <Media src={baseUrl} />
-        </MKBox>
-        <MKBox>
-          <Media src={baseUrl} />
-        </MKBox>
-        <MKBox>
-          <Media src={baseUrl} />
-        </MKBox>
+    <MKBox className="slider-container" p={{ lg: 4 }} borderRadius="2rem" bgColor="white" ml={2}>
+      <Slider {...settings} height={{ lg: 10 }}>
+        <div>
+          <img height="100%" width="100%" src={baseUrl} />
+        </div>
+        <div>
+          <img height="100%" width="100%" src={baseUrl} />
+        </div>
+        <div>
+          <img height="100%" width="100%" src={baseUrl} />
+        </div>
+        <div>
+          <img height="100%" width="100%" src={baseUrl} />
+        </div>
       </Slider>
     </MKBox>
   );
@@ -44,6 +66,18 @@ function ImageSliderComponent({ baseUrl }) {
 
 ImageSliderComponent.propTypes = {
   baseUrl: PropTypes.any,
+};
+
+NextArrow.propTypes = {
+  className: PropTypes.string,
+  style: PropTypes.object,
+  onClick: PropTypes.func,
+};
+
+PrevArrow.propTypes = {
+  className: PropTypes.string,
+  style: PropTypes.object,
+  onClick: PropTypes.func,
 };
 
 export default ImageSliderComponent;
--- src/layouts/pages/AnimalsDetailsPage/index.js
+++ src/layouts/pages/AnimalsDetailsPage/index.js
@@ -1,33 +1,89 @@
-import Grid from "@mui/material/Grid";
-import React, { useContext } from "react";
+import { Grid, Rating } from "@mui/material";
+import React, { useContext, useState } from "react";
 import ImageSliderComponent from "components/ImageSliderComponent";
 import MKBox from "components/MKBox";
-// import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+import MKTypography from "components/MKTypography";
+import MKButton from "components/MKButton";
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
 import AnimalContext from "_helper/animal";
-// import routes from "routes.prod";
+import routes from "routes.prod";
 
 function AnimalDetailsPage() {
   const { selectedAnimal } = useContext(AnimalContext);
+  const [value, setValue] = useState(0);
   console.log(selectedAnimal);
 
   return (
     <>
-      <MKBox width="100%" mx="auto" position="relative" zIndex={2}>
+      <MKBox position="fixed" top="0.5rem" width="100%">
+        <DefaultNavbar
+          routes={routes}
+          action={{
+            type: "external",
+            route: "https://www.creative-tim.com/product/material-kit-react",
+            label: "free download",
+            color: "info",
+          }}
+        />
+      </MKBox>
+      <MKBox width="100%" mt="8rem" position="relative" zIndex={2}>
         <Grid container spacing={3} alignItems="center">
-          <Grid item xs={12} lg={6}>
+          <Grid item xs={12} sm={12} md={5} lg={6} xl={6} p={{ lg: "20px", xl: "20px" }}>
             <ImageSliderComponent baseUrl="https://firebasestorage.googleapis.com/v0/b/livestock-marketplace.appspot.com/o/animals%2Fpigs_whatsapp%20image%202023-11-15%20at%2010.47.14%20am.jpeg?alt=media&token=9a9ed5ed-611c-42f4-8a2d-5111a630cdd5" />
           </Grid>
           <Grid
             item
             xs={12}
-            sm={10}
+            sm={12}
             md={7}
             lg={6}
-            xl={4}
-            ml={{ xs: "auto", lg: 6 }}
-            mr={{ xs: "auto", lg: 6 }}
+            xl={6}
+            ml={{ xs: "auto" }}
+            mr={{ xs: "auto" }}
+            sx={{ display: "flex", flexDirection: "column", justifyContent: "flex-start" }}
           >
-            other side{" "}
+            <MKTypography variant="h1">Animal Name</MKTypography>
+            <MKTypography variant="subtitle" mt={1}>
+              Slight description on the animal
+            </MKTypography>
+            <MKBox
+              my={2}
+              sx={{
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}
+            >
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+                size="large"
+                mr={2}
+                p={1}
+              />
+              <MKTypography variant="subtitle">75 Reviews</MKTypography>
+            </MKBox>
+            <MKTypography variant="h1" py={1} mt={2}>
+              $145
+            </MKTypography>
+            <MKBox
+              my={2}
+              sx={{
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                alignItems: "center",
+              }}
+            >
+              <MKButton type="submit" variant="gradient" color="info">
+                Send Message
+              </MKButton>
+              <MKButton type="submit" variant="gradient" color="info">
+                Send Message
+              </MKButton>
+            </MKBox>
           </Grid>
         </Grid>
       </MKBox>
