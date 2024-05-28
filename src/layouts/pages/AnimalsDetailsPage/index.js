0c4e7bffaec5b026c93d3413e15d98f85503b826
Isheanesu Gwangwanyu <innocentnyamusa@gmail.com>
2024-05-28 18:24:58 +0200

    updated details page

--- src/layouts/pages/AnimalsDetailsPage/index.js
+++ src/layouts/pages/AnimalsDetailsPage/index.js
@@ -1,5 +1,6 @@
-import { Grid, Rating } from "@mui/material";
 import React, { useContext, useState } from "react";
+import { Grid, Rating } from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import ImageSliderComponent from "components/ImageSliderComponent";
 import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
@@ -15,7 +16,7 @@ function AnimalDetailsPage() {
 
   return (
     <>
-      <MKBox position="fixed" top="0.5rem" width="100%">
+      <MKBox position="fixed" top="0.5rem" width="100%" zIndex={2}>
         <DefaultNavbar
           routes={routes}
           action={{
@@ -26,23 +27,25 @@ function AnimalDetailsPage() {
           }}
         />
       </MKBox>
-      <MKBox width="100%" mt="8rem" position="relative" zIndex={2}>
-        <Grid container spacing={3} alignItems="center">
-          <Grid item xs={12} sm={12} md={5} lg={6} xl={6} p={{ lg: "20px", xl: "20px" }}>
+      <MKBox width="100%" mt="8rem" position="relative">
+        <Grid container alignItems="center">
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6} p={{ xs: 1, lg: 2, xl: 2 }}>
             <ImageSliderComponent baseUrl="https://firebasestorage.googleapis.com/v0/b/livestock-marketplace.appspot.com/o/animals%2Fpigs_whatsapp%20image%202023-11-15%20at%2010.47.14%20am.jpeg?alt=media&token=9a9ed5ed-611c-42f4-8a2d-5111a630cdd5" />
           </Grid>
           <Grid
-            item
             xs={12}
             sm={12}
-            md={7}
+            md={6}
             lg={6}
             xl={6}
             ml={{ xs: "auto" }}
             mr={{ xs: "auto" }}
             sx={{ display: "flex", flexDirection: "column", justifyContent: "flex-start" }}
+            p={{ xs: 1, lg: 2, xl: 2 }}
           >
-            <MKTypography variant="h1">Animal Name</MKTypography>
+            <MKTypography variant="h1" my={{ xs: 1, sm: 1.5, md: 1, lg: 1.5 }}>
+              Animal Name
+            </MKTypography>
             <MKTypography variant="subtitle" mt={1}>
               Slight description on the animal
             </MKTypography>
@@ -61,7 +64,7 @@ function AnimalDetailsPage() {
                   setValue(newValue);
                 }}
                 size="large"
-                mr={2}
+                sx={{ marginRight: "calc(100%/20)" }}
                 p={1}
               />
               <MKTypography variant="subtitle">75 Reviews</MKTypography>
@@ -73,15 +76,21 @@ function AnimalDetailsPage() {
               my={2}
               sx={{
                 flexDirection: "row",
-                justifyContent: "flex-start",
                 alignItems: "center",
               }}
+              justifyContent={{ xs: "space-between" }}
             >
-              <MKButton type="submit" variant="gradient" color="info">
-                Send Message
+              <MKButton
+                type="submit"
+                variant="gradient"
+                color="warning"
+                width={{ xs: 100 }}
+                sx={{ marginRight: 2 }}
+              >
+                Add to Your Cart&nbsp; <AddShoppingCartIcon />
               </MKButton>
-              <MKButton type="submit" variant="gradient" color="info">
-                Send Message
+              <MKButton variant="gradient" width={{ xs: 100 }}>
+                add to wishlist
               </MKButton>
             </MKBox>
           </Grid>
