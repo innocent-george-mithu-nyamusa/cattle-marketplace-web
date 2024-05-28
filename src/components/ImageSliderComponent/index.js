b102710ee8e6845ba99c652726d503b3bec94406
Isheanesu Gwangwanyu <innocentnyamusa@gmail.com>
2024-05-28 18:24:43 +0200

    changed items in slider

--- src/components/ImageSliderComponent/index.js
+++ src/components/ImageSliderComponent/index.js
@@ -3,34 +3,40 @@ import PropTypes from "prop-types";
 // import { Media } from "reactstrap";
 import MKBox from "components/MKBox";
 
-function NextArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", textColor: "red", "&::Before": { color: "red" } }}
-      onClick={onClick}
-    />
-  );
-}
+// function NextArrow(props) {
+//   const { className, style, onClick } = props;
+//   return (
+//     <div
+//       className={className}
+//       style={{ ...style, display: "block", textColor: "red", "&::Before": { color: "red" },  }}
+//       onClick={onClick}
+//     />
+//   );
+// }
 
-function PrevArrow(props) {
-  const { className, style, onClick } = props;
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: "block", textColor: "green", "&::Before": { color: "red" } }}
-      onClick={onClick}
-    />
-  );
-}
+// function PrevArrow(props) {
+//   const { className, style, onClick } = props;
+//   return (
+//     <div
+//       className={className}
+//       style={{ ...style, display: "block", textColor: "green", "&::Before": { color: "red" } }}
+//       onClick={onClick}
+//     />
+//   );
+// }
 
 function ImageSliderComponent({ baseUrl }) {
   const settings = {
     customPaging: function (i) {
       return (
         <a>
-          <img height="100%" width="100%" key={i} src={`${baseUrl}`} />
+          <img
+            height="100%"
+            width="100%"
+            style={{ borderRadius: "4%" }}
+            key={i}
+            src={`${baseUrl}`}
+          />
         </a>
       );
     },
@@ -40,24 +46,30 @@ function ImageSliderComponent({ baseUrl }) {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
+    // nextArrow: <NextArrow />,
+    // prevArrow: <PrevArrow />,
   };
 
   return (
-    <MKBox className="slider-container" p={{ lg: 4 }} borderRadius="2rem" bgColor="white" ml={2}>
+    <MKBox
+      className="slider-container"
+      p={{ xs: 1, lg: 4 }}
+      borderRadius="2%"
+      // bgColor="white"
+      m={{ xs: "0px auto", lg: "25px auto" }}
+    >
       <Slider {...settings} height={{ lg: 10 }}>
         <div>
-          <img height="100%" width="100%" src={baseUrl} />
+          <img height="100%" width="100%" style={{ borderRadius: "4%" }} src={baseUrl} />
         </div>
         <div>
-          <img height="100%" width="100%" src={baseUrl} />
+          <img height="100%" width="100%" style={{ borderRadius: "4%" }} src={baseUrl} />
         </div>
         <div>
-          <img height="100%" width="100%" src={baseUrl} />
+          <img height="100%" width="100%" style={{ borderRadius: "4%" }} src={baseUrl} />
         </div>
         <div>
-          <img height="100%" width="100%" src={baseUrl} />
+          <img height="100%" width="100%" style={{ borderRadius: "4%" }} src={baseUrl} />
         </div>
       </Slider>
     </MKBox>
@@ -68,16 +80,16 @@ ImageSliderComponent.propTypes = {
   baseUrl: PropTypes.any,
 };
 
-NextArrow.propTypes = {
-  className: PropTypes.string,
-  style: PropTypes.object,
-  onClick: PropTypes.func,
-};
+// NextArrow.propTypes = {
+//   className: PropTypes.string,
+//   style: PropTypes.object,
+//   onClick: PropTypes.func,
+// };
 
-PrevArrow.propTypes = {
-  className: PropTypes.string,
-  style: PropTypes.object,
-  onClick: PropTypes.func,
-};
+// PrevArrow.propTypes = {
+//   className: PropTypes.string,
+//   style: PropTypes.object,
+//   onClick: PropTypes.func,
+// };
 
 export default ImageSliderComponent;
