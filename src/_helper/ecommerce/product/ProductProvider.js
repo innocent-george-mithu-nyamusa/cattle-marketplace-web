52df28b7b3dac39e7bbc31f14518fb749601d252
Innocent George Mithu Nyamusa <innocentnyamusa@gmail.com>
2024-10-02 18:41:31 +0200

    added product provider

---
+++ src/_helper/ecommerce/product/ProductProvider.js
@@ -0,0 +1,38 @@
+import Context from './index';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { ProductsApi } from '../../../api';
+
+const ProductProvider = (props) => {
+  const [productItem, setProductItem] = useState([]);
+  const symbol = '$';
+  const [singleItem, setSingleItems] = useState([]);
+  const [isVertical, setIsVertical] = useState(false);
+
+  const fetchProducts = async () => {
+    try {
+      await axios.get(ProductsApi).then((resp) => {
+      
+        setProductItem(resp.data);
+      });
+    } catch (error) {
+    }
+  };
+  const getSingleItem = () => {
+    setSingleItems(productItem[0]);
+  };
+  
+  useEffect(() => {
+    // fetchProducts();
+    // getSingleItem();
+   
+  });
+
+  return (
+    <Context.Provider value={{ ...props, productItem, singleItem, symbol, isVertical, setIsVertical }}>
+      {props.children}
+    </Context.Provider>
+  );
+};
+
+export default ProductProvider;
